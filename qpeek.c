f7877986bd4cb863921ace8789cf623813c4da48
Anthony Car <agcarat@gmail.com>
2016-05-10 23:48:43 -0400

    qpeek now requires arg offset from head of queue

--- qpeek.c
+++ qpeek.c
@@ -23,39 +23,46 @@
 #include "queueutils.h"
 
 int main(int argc, char **argv) {
-  struct Queue * q;
-  struct QueueData d;
-  int64_t l = 0;
-  int64_t j = 0;
-  char *cq = NULL;
-  int opt = 0;
+    struct Queue * q;
+    struct QueueData d;
+    int64_t l = 0;
+    int64_t j = 0;
+    char *cq = NULL;
+    int opt = 0;
 
-  while((opt = getopt(argc, argv, "hq:")) != -1)
-    switch(opt) {
-      case 'q':
-        cq = strdup(optarg);
-        break;
-      default:
-      case 'h':
-        puts("Usage: qpeek [-h] [-q queue-name] [--] <args>");
+    while((opt = getopt(argc, argv, "hq:")) != -1)
+        switch(opt) {
+            case 'q':
+                cq = strdup(optarg);
+                break;
+            default:
+            case 'h':
+                puts("Usage: qpeek [-h] [-q queue-name] [--] <args>");
+                return EXIT_FAILURE;
+        }
+    int i = optind-1;
+    if((q= queue_open( SELECTQUEUE(cq))) == NULL) {
+        puts("Failed to open the queue.");
         return EXIT_FAILURE;
     }
-
-  if((q= queue_open( SELECTQUEUE(cq))) == NULL) {
-    puts("Failed to open the queue.");
-    return EXIT_FAILURE;
-  }
-  if(queue_len(q, &l) != LIBQUEUE_SUCCESS) {
-    puts("Failed to read the queue length.");
-    closequeue(q);
-    return EXIT_FAILURE;
-  }
-  if(queue_peek(q, j-1, &d) != LIBQUEUE_SUCCESS) {
-      printf("Failed to peek at element #%lld\n",(long long) j);
-  }
-  else
-      printf("%s\n", (const char*)d.v);
-  if(cq != NULL)
-    free(cq);
-  return closequeue(q);
+    if(queue_len(q, &l) != LIBQUEUE_SUCCESS) {
+        puts("Failed to read the queue length.");
+        closequeue(q);
+        return EXIT_FAILURE;
+    }
+    while(argv[++i]) {
+        if((j = (int64_t)atoi((const char*)argv[i])) < 0
+                || (j+1)>l) {
+            printf("Index out of bounds: %lld (%lld)\n", (long long)j, (long long)l);
+            continue;
+        }
+        if(queue_peek(q, j-1, &d) != LIBQUEUE_SUCCESS) {
+            printf("Failed to peek at element #%lld\n",(long long) j);
+        }
+        else
+            printf("%s\n", (const char*)d.v);
+        if(cq != NULL)
+            free(cq);
+    }
+    return closequeue(q);
 }
